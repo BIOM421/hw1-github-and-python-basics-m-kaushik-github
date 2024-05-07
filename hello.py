fd2319bc4b5015d1784657d95db0e8d4928f82ad
m-kaushik-github <168742868+m-kaushik-github@users.noreply.github.com>
2024-05-07 21:05:18 +0530

    git commit -m "Updated hello.py"

--- hello.py
+++ hello.py
@@ -1,16 +1,7 @@
 def hello_world(): #Function to return specified string
     return "Hello World!" #Returning the string
-result = hello_world() #Storing the returned string in a variable 
-print (result) #Printing the variable 
 
 def hello_world_n(N):
     N = int(N)  # Convert string N to int N
-    result_1 = "Hello World! " * N  # Perform string multiplication
-    return result_1
-
-# Ask user for input N
-N = input("Enter your argument [integer only]: ")    
-
-# Call the function after taking input N and print the result
-result_1 = hello_world_n(N)
-print(result_1)
+    result_1 = "Hello World! " * (N-1) + "Hello World!"   # Perform string multiplication
+    return result_1  # Return the result
